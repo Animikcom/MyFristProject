478e9f664a3c04df27678f0decd0f0a5918f9eb9
Animikcom <animikcom@gmail.com>
2022-12-10 00:54:43 +0500

    MyFirstProject

---
+++ myfirst/apps/articles/views.py
@@ -0,0 +1,29 @@
+
+from django.http import Http404, HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse
+from .models import Article, Comment
+
+
+def index(request):
+    latest_articles_list = Article.objects.order_by('-pub_date')[:5]
+    return render(request, 'articles/list.html', {'latest_articles_list': latest_articles_list})
+
+def detail(request, articel_id):
+    try:
+        a= Article.objects.get (id = articel_id)
+    except:
+        raise Http404 ("Статя не найдена")
+
+    latest_comments_list = a.comment_set.order_by('-id')[:5]
+
+    return render (request, 'articles/detail.html', {'article':a, 'latest_comments_list':latest_comments_list})
+
+def leave_comment(request, articel_id):
+        try:
+           a= Article.objects.get (id = articel_id)
+        except:
+            raise Http404 ("Статя не найдена")
+        a.comment_set.create(author_name = request.POST['name'], comment_text = request.POST['text'])
+        return HttpResponseRedirect(reverse('articles:detail', args=(a.id,)))
+
